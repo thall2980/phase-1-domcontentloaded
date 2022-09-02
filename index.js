06ba0e85ad8ce8f932a8529d38fce9b6f4638bae
Tyler Hall <thall2980@gmail.com>
2022-09-02 18:48:12 -0400

    commit tree 806d23fe1062d0ce4d7d1e3f6abca79e70ff19d8

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", (e) => {
+  const targetVar = document.querySelector("#text");
+  targetVar.textContent = "This is really cool!";
+});
